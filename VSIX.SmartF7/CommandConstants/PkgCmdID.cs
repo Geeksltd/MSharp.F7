b4121756119cc94b60b03632cc247002ac4b5e5f
alireza tabatabaee <sa.tabatabaee@gmail.com>
2017-10-20 19:17:19 +0330

    Completed Commit

--- VSIX.SmartF7/CommandConstants/PkgCmdID.cs
+++ VSIX.SmartF7/CommandConstants/PkgCmdID.cs
@@ -8,7 +8,6 @@ namespace Geeks.GeeksProductivityTools
         public const uint CmdOpenInMSharp = 0x108;
         public const uint CmdOpenInMSharpAspx = 0x109;
         public const uint CmdRunBatchFile = 0x10a;
-        public const uint CmdTrimBlankLines = 0x10b;
         public const uint CmdOrganizeUsing = 0x10c;
         public const uint CmdOrganizeUsingSlnLevel = 0x10d;
         public const uint CmdCompileTsFiles = 0x10e;
